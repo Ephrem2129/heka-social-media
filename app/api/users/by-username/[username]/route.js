c5605f21698ebc42d049f2069117aa3074b78e6f
EPAY <ephremayalew486@gmail.com>
2025-01-29 20:21:19 +0300

    Add loading states

--- app/api/users/by-username/[username]/route.js
+++ app/api/users/by-username/[username]/route.js
@@ -28,3 +28,5 @@ export async function GET(_req, { params }) {
 // Modified: 2026-04-15 20:23:46 - Create logout endpoint
 
 // Modified: 2026-04-15 20:23:46 - Implement user avatar cropping
+
+// Modified: 2026-04-15 20:23:46 - Add loading states
