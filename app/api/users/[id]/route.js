d60714b22aa563639a5b5930cde01746c5cdeabb
EPAY <ephremayalew486@gmail.com>
2025-01-29 21:21:34 +0300

    Create post detail page

--- app/api/users/[id]/route.js
+++ app/api/users/[id]/route.js
@@ -82,3 +82,5 @@ export async function PATCH(req, { params }) {
     return NextResponse.json({ error: "Server error" }, { status: 500 });
   }
 }
+
+// Modified: 2026-04-15 20:23:46 - Create post detail page
