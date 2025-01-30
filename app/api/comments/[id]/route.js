7a24e66952da5e76407c0c1c5c7284cecdb657df
EPAY <ephremayalew486@gmail.com>
2025-01-30 19:13:47 +0300

    Create theme toggle component

--- app/api/comments/[id]/route.js
+++ app/api/comments/[id]/route.js
@@ -66,3 +66,5 @@ export async function DELETE(req, { params }) {
 // Modified: 2026-04-15 20:23:46 - Create register API endpoint
 
 // Modified: 2026-04-15 20:23:46 - Fix authentication token expiry
+
+// Modified: 2026-04-15 20:23:47 - Create theme toggle component
