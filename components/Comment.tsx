17eae08e60972cf34274afa4aa436d76b58994c0
EPAY <ephremayalew486@gmail.com>
2025-02-13 17:41:33 +0300

    Implement activity feed

--- components/Comment.tsx
+++ components/Comment.tsx
@@ -115,3 +115,5 @@ export default function Comment({ comment, me, onReply, onEdit, onDelete, postId
 // Modified: 2026-04-15 20:23:46 - Add password strength validation
 
 // Modified: 2026-04-15 20:23:47 - Create following list endpoint
+
+// Modified: 2026-04-15 20:23:47 - Implement activity feed
