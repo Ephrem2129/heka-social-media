19248e7912651d179d9f784b5a5ff190b05fe8e9
EPAY <ephremayalew486@gmail.com>
2025-02-14 09:18:19 +0300

    Fix follow relationship queries

--- app/api/posts/[id]/route.js
+++ app/api/posts/[id]/route.js
@@ -121,3 +121,5 @@ export async function DELETE(req, { params }) {
 // Modified: 2026-04-15 20:23:46 - Create hashtag parsing
 
 // Modified: 2026-04-15 20:23:46 - Add post search functionality
+
+// Modified: 2026-04-15 20:23:47 - Fix follow relationship queries
